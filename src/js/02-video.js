0157dadd020a79fba90ad096eff8f8dbd6ebf39d
Andrew Butenko <sedkol88@gmail.com>
2023-11-12 01:04:25 +0200

    task2 is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const playerEl = new Player(iframe);
+
+//----------------------------------------------------------------------
+const saveTime = throttle(function() {
+    // Отримати поточний час відтворення відео
+    playerEl.getCurrentTime().then(function(time) {
+        let videoTime = time;
+
+        // Зберегти час відтворення відео в локальному сховищі
+        localStorage.setItem('videoplayer-current-time', videoTime);
+    });
+}, 1000);
+
+//------------------------------------------------------------------------
+playerEl.on('timeupdate', saveTime);
