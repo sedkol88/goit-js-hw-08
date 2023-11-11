10ddc0ed81d7747d13ce55e667c0f405e867b18e
Andrew Butenko <sedkol88@gmail.com>
2023-11-11 22:14:54 +0200

    task1 is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -13,31 +13,19 @@ console.log(galleryItems);
 const list = document.querySelector(".gallery");
 
 const markup = galleryItems
-  .map((item) =>
-    `<li class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-    </a>
-    </li>`
+  .map((item) =>       
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+        </li>`
   )
   .join("");
 
 list.insertAdjacentHTML("beforeend", markup);
 
-list.addEventListener("click", handleClick);
-
-function handleClick(event) {
-    event.preventDefault();
-
-    if (!event.target.classList.contains('gallery__image')) {
-      return;
-    }
-
-  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" >`, options);
-  instance.show();
-};
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+    });
