853e9f7837403ce54202db2c019ed4ab5fff9b90
Andrew Butenko <sedkol88@gmail.com>
2023-11-11 06:34:35 +0200

    add simplelightbox

---
+++ src/js/01-gallery.js
@@ -0,0 +1,10 @@
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
